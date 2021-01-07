afe7d339118bcbec5e20cd0b113221cc59f521d5
Lanuvelza <ianlam1221@hotmail.com>
2021-01-07 22:56:07 +0000

    takes an object and a callback. Returns the first key for which the callback returns truthy value. If no key is found, then it should return undefined

---
+++ findKey.js
@@ -0,0 +1,53 @@
+let correct = String.fromCodePoint(0x1F7E2);
+let wrong = String.fromCodePoint(0x1F534);
+
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`${correct}${correct}${correct} Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`${wrong}${wrong}${wrong} Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4); // => "noma"
+
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "noma"
+
+
+
+assertEqual(result1, "noma");
+assertEqual(result2, undefined);
+assertEqual(result3, "Akaleri");
