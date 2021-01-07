f119b8003b96fa29334a8fd4825072998a36a4be
Lanuvelza <ianlam1221@hotmail.com>
2021-01-07 00:05:42 +0000

    evaluates if two objects are the same

---
+++ eqObjects.js
@@ -0,0 +1,61 @@
+let correct = String.fromCodePoint(0x1F7E2);
+let wrong = String.fromCodePoint(0x1F534);
+
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`${correct}${correct}${correct} Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`${wrong}${wrong}${wrong} Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  let boolean = false;
+  if (arr1.length === arr2.length) {
+    boolean = true;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        boolean = true;
+      } else {
+        return false;
+      }
+    }
+  }
+  return boolean;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  let boolean = true; 
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+
+  if (key1.length !== key2.length) {
+    boolean = false;
+  }
+  
+  for (let key of key1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      boolean = eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key] && object1[key].length !== object2[key].length) {
+      boolean = false;
+    }
+  }
+  return boolean;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
