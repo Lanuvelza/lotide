29678d5336e493db712506b3ee1aa1def6b3a0fe
Lanuvelza <ianlam1221@hotmail.com>
2021-01-11 23:12:32 +0000

    refactored to use mocha and chai functions

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,14 +1,45 @@
-const assertEqual = require('../assertEqual'); 
+const assert = require('chai').assert; 
 const eqArrays = require('../eqArrays');
 
 
+
 // Test cases for eqArrays 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([[2,3],[4]], [[2,3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+describe("#eqArrays", () => {
+
+  it("returns true for comparing [1, 2, 3] with [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false for comparing [1, 2, 3] with [3, 2, 1]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns true for comparing [1, 2, 3] with [1, 2]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2]), false);
+  });
+
+  it("returns true for comparing ['1', '2', '3'] with ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  it("returns false for comparing ['1', '2', '3'] with ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  it("returns true for comparing [] with []", () => {
+    assert.strictEqual(eqArrays([],[]), true);
+  }); 
+
+  it("returns false for comparing [[2, 3], [4]] with [[2, 3], [4, 5]]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+
+  it("returns true for comparing [[2, 3], [4]] with [[2, 3], [4]]", () => {
+    assert.strictEqual(eqArrays([[2,3],[4]], [[2,3], [4]]), true);
+  });
+
+  it("returns false for comparing [[2, 3], [4]] with [[2, 3], 4]", () => {
+    assert.strictEqual(eqArrays([[2,3],[4]], [[2,3], 4]), false);
+  });
+
+});
