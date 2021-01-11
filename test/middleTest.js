95e57cb0081dab718fffcb02b05e3687a03a74dc
Lanuvelza <ianlam1221@hotmail.com>
2021-01-11 20:28:45 +0000

    test cases for middle function

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// Test cases for middle 
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
