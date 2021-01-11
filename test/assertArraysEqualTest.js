1c2bbd9056bbd80799e83f97a2f41bd03b2b0008
Lanuvelza <ianlam1221@hotmail.com>
2021-01-11 20:22:40 +0000

    test cases for assertArraysEqualTest function

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2]);
+assertArraysEqual([], []);
