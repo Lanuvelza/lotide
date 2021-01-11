464a077465e4461240e4be2add78a2085adf8a85
Lanuvelza <ianlam1221@hotmail.com>
2021-01-11 19:55:25 +0000

    test cases for assertEqual function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// Test code for assertEqual.js 
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("hello", "hello");
+assertEqual(1,2);
+
